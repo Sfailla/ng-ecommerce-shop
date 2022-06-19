928f8ef487e5c8b8167b30d6ce90cd8b84f0bae6
Sfailla <Sfailla1983@gmail.com>
2022-06-19 15:30:28 -0400

    refactor helper fns. add types for helper fns

--- libs/core/src/lib/utils/helperFns.ts
+++ libs/core/src/lib/utils/helperFns.ts
@@ -6,6 +6,7 @@ import { ApiResponse, ApiError } from '../types'
 interface SuccessConfig {
   redirectTo?: string | null
   setState?: (() => void) | void | null
+  message?: string
 }
 
 type SuccessResponse<T> = (response: ApiResponse<T>) => void
@@ -14,29 +15,31 @@ type SuccessResponse<T> = (response: ApiResponse<T>) => void
 export class HelperFns {
   constructor(private messageService: ToastMessageService, private router: Router) {}
 
-  handleSuccess<T>({ redirectTo = null, setState }: SuccessConfig = {}): SuccessResponse<T> {
-    return (response: ApiResponse<T>) => {
-      if (response.message) {
-        this.messageService.handleToastMessage(
-          ToastMessageSeverity.Success,
-          ToastMessageSummary.Success,
-          response.message
-        )
-      } else if (setState) {
-        setState()
-      } else if (redirectTo) {
-        setTimeout(() => this.router.navigateByUrl(redirectTo), 1500)
-      }
-    }
+  private toastMessage(message: string) {
+    return this.messageService.handleToastMessage(
+      ToastMessageSeverity.Success,
+      ToastMessageSummary.Success,
+      message
+    )
   }
 
-  handleError = () => {
-    return (error: ApiError) => {
-      this.messageService.handleToastMessage(
-        ToastMessageSeverity.Error,
-        ToastMessageSummary.Error,
-        error.message
-      )
+  private buildResponse({ message, setState, redirectTo }: SuccessConfig): void {
+    if (message) this.toastMessage(message)
+    else if (setState) setState()
+    else if (redirectTo) setTimeout(() => this.router.navigateByUrl(redirectTo), 1500)
+  }
+
+  public handleSuccess =
+    <T>({ redirectTo = null, setState }: SuccessConfig = {}): SuccessResponse<T> =>
+    ({ message }: ApiResponse<T>) => {
+      this.buildResponse({ message, setState, redirectTo })
     }
+
+  public handleError = () => (error: ApiError) => {
+    this.messageService.handleToastMessage(
+      ToastMessageSeverity.Error,
+      ToastMessageSummary.Error,
+      error.message
+    )
   }
 }
